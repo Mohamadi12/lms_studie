b22f5dccf5609511b5fda689ea4bb3955cedd34c
Nana Mohamadi <adilnana78@gmail.com>
2024-11-20 21:23:42 +0100

    Pricing Plans That Fit Your Right(card de payement)

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import CallToAction from './_components/call-to-action'
-import DashboardSnippet from './_components/dashboard-snippet'
+import React from "react";
+import CallToAction from "./_components/call-to-action";
+import DashboardSnippet from "./_components/dashboard-snippet";
+import dynamic from "next/dynamic";
+
+const PricingSection = dynamic(
+  () =>
+    import("./_components/pricing").then(
+      (component) => component.PricingSection,
+    ),
+  { ssr: true },
+)
 
 const Home = () => {
   return (
     <div className="md:px-10 py-20 flex flex-col gap-36">
       <div>
-        <CallToAction/>
-        <DashboardSnippet/>
+        <CallToAction />
+        <DashboardSnippet />
       </div>
+      <PricingSection />
     </div>
-  )
-}
+  );
+};
 
 export default Home
