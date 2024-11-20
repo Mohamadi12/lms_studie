7b601469623a244bc8b05ecfee8e0b19534ea684
Nana Mohamadi <adilnana78@gmail.com>
2024-11-20 20:47:25 +0100

    Interface section image community

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CallToAction from './_components/call-to-action'
+import DashboardSnippet from './_components/dashboard-snippet'
 
 const Home = () => {
   return (
     <div className="md:px-10 py-20 flex flex-col gap-36">
       <div>
         <CallToAction/>
+        <DashboardSnippet/>
       </div>
     </div>
   )
