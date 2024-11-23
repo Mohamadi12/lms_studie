5b354cdc0b11874ae81fb283c586c3d57721c834
Nana Mohamadi <adilnana78@gmail.com>
2024-11-23 20:22:55 +0100

    Group settings qui prends en compte images,photos

--- src/hooks/groups/index.ts
+++ src/hooks/groups/index.ts
@@ -1,13 +1,25 @@
 "use client";
 
-import { onSearchGroups } from "@/src/actions/groups";
+import {
+  onGetGroupInfo,
+  onSearchGroups,
+  onUpDateGroupSettings,
+} from "@/src/actions/groups";
 import { superbaseClient } from "@/src/lib/utils";
 import { onOnline } from "@/src/redux/slices/online-member-slice";
 import { onClearSearch, onSearch } from "@/src/redux/slices/search-slice";
 import { AppDispatch } from "@/src/redux/store";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
+import { JSONContent } from "novel";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { GroupSettingsSchema } from "@/src/components/forms/group-settings/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { upload } from "@/src/lib/uploadcare";
 
 export const useGroupChatOnline = (userid: string) => {
   const dispatch: AppDispatch = useDispatch();
@@ -97,4 +109,171 @@ export const useSearch = (search: "GROUPS" | "POSTS") => {
   }, [debounce]);
 
   return { query, onSearchQuery };
+};
+
+export const useGroupSettings = (groupid: string) => {
+  const { data } = useQuery({
+    queryKey: ["group-info"],
+    queryFn: () => onGetGroupInfo(groupid),
+  });
+
+  const jsonContent = data?.group?.jsonDescription
+    ? JSON.parse(data?.group?.jsonDescription as string)
+    : undefined;
+
+  const [onJsonDescription, setJsonDescription] = useState<
+    JSONContent | undefined
+  >(jsonContent);
+
+  const [onDescription, setOnDescription] = useState<string | undefined>(
+    data?.group?.description || undefined
+  );
+
+  const {
+    register,
+    formState: { errors },
+    reset,
+    handleSubmit,
+    watch,
+    setValue,
+  } = useForm<z.infer<typeof GroupSettingsSchema>>({
+    resolver: zodResolver(GroupSettingsSchema),
+    mode: "onChange",
+  });
+  const [previewIcon, setPreviewIcon] = useState<string | undefined>(undefined);
+  const [previewThumbnail, setPreviewThumbnail] = useState<string | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    const previews = watch(({ thumbnail, icon }) => {
+      if (!icon) return;
+      if (icon[0]) {
+        setPreviewIcon(URL.createObjectURL(icon[0]));
+      }
+      if (thumbnail[0]) {
+        setPreviewThumbnail(URL.createObjectURL(thumbnail[0]));
+      }
+    });
+    return () => previews.unsubscribe();
+  }, [watch]);
+
+  const onSetDescriptions = () => {
+    const JsonContent = JSON.stringify(onJsonDescription);
+    setValue("jsondescription", JsonContent);
+    setValue("description", onDescription);
+  };
+
+  useEffect(() => {
+    onSetDescriptions();
+    return () => {
+      onSetDescriptions();
+    };
+  }, [onJsonDescription, onDescription]);
+  const { mutate: update, isPending } = useMutation({
+    mutationKey: ["group-settings"],
+    mutationFn: async (values: z.infer<typeof GroupSettingsSchema>) => {
+      if (values.thumbnail && values.thumbnail.length > 0) {
+        const uploaded = await upload.uploadFile(values.thumbnail[0]);
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "IMAGE",
+          uploaded.uuid,
+          `/group/${groupid}/settings`
+        );
+        if (updated.status !== 200) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty",
+          });
+        }
+      }
+      if (values.icon && values.icon.length > 0) {
+        console.log("icon");
+        const uploaded = await upload.uploadFile(values.icon[0]);
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "ICON",
+          uploaded.uuid,
+          `/group/${groupid}/settings`
+        );
+        if (updated.status !== 200) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty",
+          });
+        }
+      }
+      if (values.name) {
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "NAME",
+          values.name,
+          `/group/${groupid}/settings`
+        );
+        if (updated.status !== 200) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty",
+          });
+        }
+      }
+      console.log("DESCRIPTION");
+
+      if (values.description) {
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "DESCRIPTION",
+          values.description,
+          `/group/${groupid}/settings`
+        );
+        if (updated.status !== 200) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty",
+          });
+        }
+      }
+      if (values.jsondescription) {
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "JSONDESCRIPTION",
+          values.jsondescription,
+          `/group/${groupid}/settings`
+        );
+        if (updated.status !== 200) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty",
+          });
+        }
+      }
+      if (
+        !values.description &&
+        !values.name &&
+        !values.thumbnail.length &&
+        !values.icon.length &&
+        !values.jsondescription
+      ) {
+        return toast("Error", {
+          description: "Oops! looks like your form is empty",
+        });
+      }
+      return toast("Success", {
+        description: "Group data updated",
+      });
+    },
+  });
+  const router = useRouter();
+  const onUpdate = handleSubmit(async (values) => update(values));
+  if (data?.status !== 200) router.push(`/group/create`);
+
+  return {
+    data,
+    register,
+    errors,
+    onUpdate,
+    isPending,
+    previewIcon,
+    previewThumbnail,
+    onJsonDescription,
+    setJsonDescription,
+    setOnDescription,
+    onDescription,
+  };
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,7 @@ export const superbaseClient = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 )
+
+export const truncateString = (string: string) =>{
+  return string.slice(0, 60) + "..."
+}
