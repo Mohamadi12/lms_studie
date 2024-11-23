ec270ec269271a2c23b025c04a3a8e1f141494c7
Nana Mohamadi <adilnana78@gmail.com>
2024-11-23 12:56:18 +0100

    Après qu'on active ça,on vera la Subscriptions,Affiliates,Domaine Config,Integration et General

---
+++ src/app/group/[groupid]/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const GroupSettingsPage = (props: Props) => {
+  return (
+    <div>GroupSettingsPage</div>
+  )
+}
+
+export default GroupSettingsPage
